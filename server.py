940297263840d8ffb1aacb805f932a91931664ef
R* <64038390+raster0x2a@users.noreply.github.com>
2026-04-30 16:24:16 +0900

    Update server.py

--- server.py
+++ server.py
@@ -8,11 +8,13 @@ print("さっそくゲームを始めよう！", flush=True)
 print("心の準備がつかない？では何度かシミュレーションをしてみるとしようか", flush=True)
 
 # 乱数でシードを決める
-seed = random.randint(1, 100)
+seed = random.randint(1, 10)
 rng = random.Random(seed)
-
+# 装填数
+N = 100
 # 10回シミュレーション
-history = [rng.randint(1, 10) for _ in range(10)]
+SIM_TIMES = 10
+history = [rng.randint(1, N) for _ in range(SIM_TIMES)]
 # ここでは1を選んだこととする、死んだ回数をカウント
 death_count = len(history) - history.count(1)
 
@@ -22,7 +24,7 @@ for i, n in enumerate(history, 1):
 
 print(f"\nもし本番なら君は {death_count} 回は死んだね", flush=True)
 print("さあ本番だ", flush=True)
-print("1〜10の数字を入力してください:", end="", flush=True)
+print(f"1〜{N}の数字を入力してください:", end="", flush=True)
 
 try:
     guess = int(input())
@@ -30,12 +32,12 @@ except Exception:
     print("入力が不正です。", flush=True)
     exit()
 
-if not 1 <= guess <= 10:
-    print("1〜10の数字を入力してください。", flush=True)
+if not 1 <= guess <= N:
+    print(f"1〜{N}の数字を入力してください。", flush=True)
     exit()
 
 # この数字を当てれば生還
-answer = rng.randint(1, 10)
+answer = rng.randint(1, N)
 
 if guess == answer:
     print("生還おめでとう！", flush=True)
